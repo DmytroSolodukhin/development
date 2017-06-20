c89bd8846eca9aff21fafe5f1081565391181989
sasharr <wyra4ydo@gmail.com>
2017-06-20 16:21:42 +0300

    Add files via upload
    sto

---
+++ sto-bogachenko/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $('.sliderparent').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
+
+});
+
+
